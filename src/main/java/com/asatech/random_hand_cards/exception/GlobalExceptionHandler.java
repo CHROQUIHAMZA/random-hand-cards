ccdafb224c3d9de59c0d11f590493fcb3a9bc1d0
Chroqui Hamza <119179258+CHROQUIHAMZA@users.noreply.github.com>
2026-07-02 01:48:58 +0100

    feat: gestion des pages introuvables (404) et documentation de sécurité
    - Ajout de l'interception de NoResourceFoundException dans le GlobalExceptionHandler pour gérer les URLs inexistantes (Erreur 404).
    - Ajout de commentaires sur les méthodes de gestion d'erreurs (400, 500).

--- src/main/java/com/asatech/random_hand_cards/exception/GlobalExceptionHandler.java
+++ src/main/java/com/asatech/random_hand_cards/exception/GlobalExceptionHandler.java
@@ -7,12 +7,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Erreur 400 (Bad Request) : Intercepte les violations de règles métier.
+     * Déclenchée par notre programmation défensive (ex: taille de paquet invalide).
+     * On transmet le message d'erreur d'origine car il contient l'explication
+     * métier précise pour aider à corriger sa requête.
+     */
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleIllegalArgumentException(IllegalArgumentException ex , HttpServletRequest request, Model model){
@@ -27,6 +34,30 @@ public class GlobalExceptionHandler {
         return "error";
     }
 
+    /**
+     * Erreur 404 (Not Found) : Gère les cas où l'utilisateur tape une URL qui n'existe pas.
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(NoResourceFoundException ex, HttpServletRequest request, Model model) {
+
+        ApiError apiError = new ApiError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "La page que vous recherchez n'existe pas ou a été déplacée.",
+                request.getRequestURI()
+        );
+
+        model.addAttribute("apiError", apiError);
+        return "error"; // On réutilise ta belle page d'erreur existante !
+    }
+
+
+    /**
+     * Erreur 500 (Internal Server Error) : Filet de sécurité (Catch-all).
+     * Intercepte tous les bugs techniques inattendus (ex: NullPointerException).
+     */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleGlobalException(Exception ex , HttpServletRequest request,Model model){
